bbd1335425db604d79e18143da4b266b539e6c69
cleoqc <cleoqc1124@gmail.com>
2016-10-20 10:19:07 -0400

    attempt to have different sections

--- Scratch_Commands/generateScratchCommands.py
+++ Scratch_Commands/generateScratchCommands.py
@@ -2,6 +2,7 @@ import svgwrite
 import json,ast
 from pprint import pprint
 from PIL import Image
+import sys
 
 def getImageSize(image_filename):
 
@@ -27,16 +28,22 @@ legendsize=(200,40)
 breathingspace=30
 x = 0
 y = 1
-print titlesize[0]
 
-with open("gopigo/scratch_commands.json") as data_file:
+robot=str(sys.argv[1]).lower()
+if not robot in ["gopigo","brickpi","grovepi"]:
+	print("Robot not recognised: {}".format(sys.argv[1]))
+	exit()
+
+
+
+with open("{}/scratch_commands.json".format(robot)) as data_file:
 	data = data_file.read()
 	parsed_commands = json.loads(data)
 
 	# use ast to remove all unicode encoding
 	parsed_commands = ast.literal_eval(json.dumps(parsed_commands))
 
-svg_document = svgwrite.Drawing(filename = "gopigo.svg",
+svg_document = svgwrite.Drawing(filename = "{}.svg".format(robot),
 								size = ("{}px".format(imagesize[x]), "{}px".format(imagesize[y])),
 								debug=True)
 
@@ -74,33 +81,43 @@ for top,content in parsed_commands.iteritems():
 			print("---")
 			posV=breathingspace+vOffset
 			print("PosV 3:",posV)
+
+			pprint (column)
+			if "StartColumn" in column:
+				print ("StartColumn title is {}".format(column["StartColumn"]))
+				# need to switch to a new column in the image
+				# print new Column title
+				svg_document.text(column["StartColumn"],insert=(posH,posV),style = "font-size:10px;")
+				posV+=titlesize[y]
+			else:
 	
-			# go into each set of blocks
-			for block in column['column']:
-				title = svg_document.text(block["SectionTitle"],insert=(posH,posV))
-				svg_document.add(title)
-				posV+=10
-				print("PosV 4:",posV)
-				for b in block["Block"]:
-					image_filename="gopigo/{}".format(b["image"])
-					imagewidth,imageheight=getImageSize(image_filename)
-					print("PosV 5:",posV)
-					img=svg_document.image(image_filename,insert=(posH,posV),size=(imagewidth,imageheight))
-					img.fit("left","top","meet")
-					svg_document.add(img)
-					posV+=(imageheight+10)
-					print("PosV 6:",posV)
-					#try:
-						#pprint(b["legend"])
-						#columnheight+=legendsize[y]
-					#except:
-						#pass
-				columnheight = posV-vOffset
-				if (columnheight) > maxcolumnheight:
-					maxcolumnheight = columnheight
-				posV+=30
-				print "maxcolumnheight:", maxcolumnheight
-			posH+=columnWidth
+				# go into each set of blocks
+				for block in column['column']:
+					title = svg_document.text(block["SectionTitle"],insert=(posH,posV),style = "font-size:18px;")
+					svg_document.add(title)
+					posV+=10
+					print("PosV 4:",posV)
+					for b in block["Block"]:
+						image_filename="{}/{}".format(robot,b["image"])
+						imagewidth,imageheight=getImageSize(image_filename)
+						print("PosV 5:",posV)
+						img=svg_document.image(image_filename,insert=(posH,posV),size=(imagewidth,imageheight))
+						img.fit("left","top","meet")
+						svg_document.add(img)
+						posV+=(imageheight+10)
+						print("PosV 6:",posV)
+						try:
+							#pprint(b["legend"])
+							legend=svg_document.text(b["legend"],insert=(posH,posV),style = "font-size:8px;")
+							posV+=(legendsize[y])
+						except:
+							pass
+					columnheight = posV-vOffset
+					if (columnheight) > maxcolumnheight:
+						maxcolumnheight = columnheight
+					posV+=30
+					print "maxcolumnheight:", maxcolumnheight
+				posH+=columnWidth
 		print("Box at {} size {}".format((10,boxV),(posH,maxcolumnheight)))
 		svg_document.add(svg_document.rect(insert=(10,boxV),size=(posH,(maxcolumnheight)),fill="white",opacity=0.10,stroke="black",rx=30,stroke_width="1"))
 	vOffset+=(maxcolumnheight+30)
